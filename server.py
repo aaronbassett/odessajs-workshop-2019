cd5960938fa5a25c0db7695de7e50c7b515b791d
Aaron Bassett <me@aaronbassett.com>
2019-07-17 14:14:03 +0200

    Create WebSocket server to count connected clients

--- server.py
+++ server.py
@@ -4,9 +4,28 @@ from logzero import logfile, logger
 logfile("/tmp/odessajs-workshop.log", maxBytes=1e6, backupCount=3)
 
 
+class WebSocketClientCounter(tornado.websocket.WebSocketHandler):
+
+    connected_clients = []
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        WebSocketClientCounter.connected_clients.append(self)
+        logger.info(f"Client connected [{len(self.connected_clients)}]")
+
+    def on_close(self):
+        WebSocketClientCounter.connected_clients.remove(self)
+        logger.warn(f"Client disconnected [{len(self.connected_clients)}]")
+
+
 if __name__ == "__main__":
     app = tornado.web.Application(
-        [(r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"})],
+        [
+            (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
+            (r"/", WebSocketClientCounter),
+        ],
         debug=True,
         autoreload=True,
     )
