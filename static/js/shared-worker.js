fe95fad79b7b966e62b1469bd5ce13db0d1e47ae
Aaron Bassett <me@aaronbassett.com>
2019-07-18 02:09:41 +0200

    Re-establish shared WebSocket connection on close

--- static/js/shared-worker.js
+++ static/js/shared-worker.js
@@ -1,25 +1,32 @@
 const workerConnections = []
 let sharedWebSocket = false
 
+function createSharedWebSocket() {
+  sharedWebSocket = new WebSocket("wss://nexmo-odessajs.ngrok.io/")
+
+  sharedWebSocket.onmessage = function(e) {
+    workerConnections.forEach(function(connection) {
+      let data = JSON.parse(e.data)
+      connection.postMessage(data)
+    })
+  }
+
+  console.log('Shared WebSocket opened')
+}
+
 onconnect = function(e) {
   const port = e.ports[0]
   workerConnections.push(port)
   port.start()
 
   if (!sharedWebSocket) {
-    sharedWebSocket = new WebSocket("wss://nexmo-odessajs.ngrok.io/")
+    createSharedWebSocket()
 
-    sharedWebSocket.onmessage = function(e) {
-      workerConnections.forEach(function(connection) {
-        let data = JSON.parse(e.data)
-        connection.postMessage(data)
-      })
+    sharedWebSocket.onclose = function(e) {
+      console.log('Shared WebSocket closed')
+      createSharedWebSocket()
     }
 
-    setInterval(function() {
-      console.log('Keeping alive')
-      sharedWebSocket.send("Keep Alive")
-    }, 5000)
   } else {
     sharedWebSocket.send("Ping")
   }
